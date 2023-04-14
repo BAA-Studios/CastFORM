ebb3fffd4761d5cee15954ecfc241c5c1110d5a5
KOOKIIEStudios <25145447+KOOKIIEStudios@users.noreply.github.com>
2023-04-14 19:46:47 +0800

    STYLE: Set app theme
    Set font family to Roboto Slab (consistent with output & pdf preview font). Add dark theme, and convert app to Material 3

--- lib/app.dart
+++ lib/app.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
-import 'screens/home_material.dart';
+import 'package:castform/screens/home_material.dart';
+import 'package:castform/style.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      /* TODO: Redactor to style.dart
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),*/
-      home: HomeMaterial(),  // Google-style for Windows
+    return MaterialApp(
+      theme: theme,
+      darkTheme: darkTheme,
+      home: const HomeMaterial(),  // Google-style for Windows
       // home: HomeCupertino(),  // Apple-style for macOS
     );
   }
--- lib/style.dart
+++ lib/style.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 
 const labelTextSize = 20.0;
-const String defaultFontName = "Roboto";
+String defaultFontName = "Roboto";
 const defaultFontWeight = FontWeight.w300;
+
+final theme = ThemeData(
+  fontFamily: defaultFontName,
+  colorSchemeSeed: Colors.blueGrey[800],
+  brightness: Brightness.light,
+  useMaterial3: true,
+);
+
+final darkTheme = ThemeData(
+  fontFamily: defaultFontName,
+  colorSchemeSeed: Colors.indigo,
+  brightness: Brightness.dark,
+  useMaterial3: true,
+);
